b727d89df3ae65a07f6693d5ecdcd5048c1e2dd5
Jae <giffordjacob0@gmail.com>
2023-04-06 08:15:03 +0800

    orders further compartmentalized into specific controllers

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
-  user: {
-    _id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    username: { type: String, required: true },
+const MessageSchema = new Schema(
+  {
+    user: {
+      _id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+      username: { type: String, required: true },
+    },
+    room: { type: Schema.Types.ObjectId, ref: 'Room', required: true },
+    message: { type: String, required: true },
+    timestamp: { type: String, required: true },
   },
-  room: {
-    /*  type: Schema.Types.ObjectId, ref: 'Room', required: true  */
-  },
-  message: { type: String, required: true },
-  timestamp: { type: String, required: true },
-});
+  { timestamps: true }
+);
 
 // Export model
 module.exports = mongoose.model('Message', MessageSchema);
--- server/models/Room.js
+++ server/models/Room.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const RoomSchema = new Schema({
-  users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
-  roomId: { type: String, required: true },
-  messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
-});
+const RoomSchema = new Schema(
+  {
+    users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    roomId: { type: String, required: true },
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
+  },
+  { timestamps: true }
+);
 
 // Export model
 module.exports = mongoose.model('Room', RoomSchema);
