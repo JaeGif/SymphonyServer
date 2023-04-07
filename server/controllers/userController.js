dab30fa43f9be1c733e4d763669724a7e37f82b8
Jae <giffordjacob0@gmail.com>
2023-04-07 18:10:06 +0800

    search for users now live

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,6 +10,16 @@ exports.user_get = async (req, res, next) => {
   user ? res.json({ user }).status(200) : res.sendStatus(400);
 };
 exports.users_get = async (req, res, next) => {
-  const users = await User.find({});
-  users ? res.json({ users }).status(200) : res.sendStatus(400);
+  let { q, l } = req.query;
+  let limit = l || 5;
+  let query = null || q;
+  /*   const users = await User.find({});
+   */
+  console.log(query);
+  if (query) {
+    const users = await User.find({
+      username: { $regex: query, $options: 'i' },
+    }).limit(limit);
+    users ? res.json({ users }).status(200) : res.sendStatus(400);
+  }
 };
